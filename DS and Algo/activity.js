60b4bee328a19bd6671d70cd85be5981abab40fe
Gc96 <111275475+Gc96@users.noreply.github.com>
2022-08-24 16:19:54 +0800

    Add files via upload

---
+++ DS and Algo/activity.js
@@ -0,0 +1,17 @@
+
+
+
+// const numbers = [1,2,3,4,5,6];
+
+let n1 = parseInt(prompt ('numbers'))
+
+let sum = 0
+
+while(n1 <1000){
+    console.log('numbers '+n1);
+    n1++;
+};
+
+console.log(sum);
+
+// console.log(numbers);
